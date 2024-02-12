d1414f3a5aca00d65bf41d5e6985b182c861f4cb
code_creator <42152517+SkullCandby@users.noreply.github.com>
2024-02-13 00:17:32 +0100

    Update HeaderTitle.vue

--- src/components/HeaderTitle.vue
+++ src/components/HeaderTitle.vue
@@ -7,7 +7,7 @@
       <img :src="imagePath2" alt="Title Image" class="header-image">
       <img :src="imagePath3" alt="Title Image" class="header-image">
     </div>
-      <p style="font-weight: bold; color:black; font-size: 2em;"> CA: HVNwzt7Pxfu76KHCMQPTLuTCLTm6WnQ1esLv4eizseSv </p>
+      <p style="font-weight: bold; color:black; font-size: 2em;"> CA: 52mE2VRk8qGmLFWmRABYvGHxMwDMCM4jnXpJPazCJ2fu </p>
     </div>
   </template>
   
